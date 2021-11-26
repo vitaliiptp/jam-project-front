247d90ef38720073f0d0956b4860fa2e03013f62
vitaliiptp <vitalii.potapenko@gmail.com>
2021-11-26 12:38:09 +0100

    final map component

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
+import axios from "axios";
+import UserModalMap from "../UserModalMap/UserModalMap";
 import dummyImg from "../../assets/profile-dummy-700x700.png";
+import './index.css'
 
 const GoogleMapsStatic = () => {
+    const [openUserModal, setOpenUserModal] = useState(false);
   const [location, setLocation] = useState({
     center: {
       lat: 52.52,
@@ -11,7 +15,30 @@ const GoogleMapsStatic = () => {
     zoom: 11,
   });
   const [showUser, setShowUser] = useState(false);
-  const [radius, setRadius] = useState(10_000);
+  const [showOtherUsers, setShowOtherUsers] = useState(false);
+    const [userId, setUserId] = useState(false);
+
+    const [otherUsersLocation, setOtherUsersLocation] = useState([]);
+  // const [radius, setRadius] = useState(10_000); // Show radius on the map (WIP)
+  // const [mapReference, setMapReference] = useState(null); // Show radius on the map (WIP)
+  // const [mapsReference, setMapsReference] = useState(null); // Show radius on the map (WIP)
+
+
+    const handleCloseUserModal = () => {
+        setOpenUserModal(false);
+    };
+
+
+    const handleModal = (id) => {
+        handleShowUserModal();
+        setUserId(id);
+    }
+
+    const handleShowUserModal = (id) => {
+        setOpenUserModal(true);
+    };
+
+
 
   const locateMe = () => {
     if (!navigator.geolocation) {
@@ -37,6 +64,51 @@ const GoogleMapsStatic = () => {
     }
   };
 
+  const showOthers = () => {
+    setShowOtherUsers(true);
+  };
+
+  // useEffect(() => {
+  //   axios.post(`/api/users/${userId}`, {
+  //     userId: userId,
+  //     userLocation: location,
+  //   })
+  //       .then((response) => {
+  //         console.log(response.data);
+  //       })
+  //       .catch((err) => {
+  //         console.log("Error logging in please try again");
+  //       });
+  // }, [showUser]);
+
+  useEffect(() => {
+      axios
+        .get("/users/location")
+        .then((response) => {
+          setOtherUsersLocation(response.data);
+          console.log(response.data);
+        })
+        .catch((err) => {
+          console.log("Error logging in please try again");
+        });
+  }, [showOtherUsers]);
+
+  // Show radius on the map (WIP)
+  // useEffect(() => {
+  //   if (mapReference && mapsReference) {
+  //     // if not null
+  //     const myCircle = new mapsReference.Circle({
+  //       strokeColor: "#FF0000",
+  //       strokeOpacity: 0.8,
+  //       strokeWeight: 2,
+  //       fillColor: "#FF0000",
+  //       fillOpacity: 0.3,
+  //       mapReference,
+  //       center: { lat: 22.704892, lng: 75.909964 },
+  //       radius: 800,
+  //     });
+  //   }
+  // }, [mapReference, mapsReference]);
 
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
@@ -47,35 +119,41 @@ const GoogleMapsStatic = () => {
           justifyContent: "center",
         }}
       >
-        <button
+        <button className="search-user-btn"
           onClick={locateMe}
-          style={{ width: "5rem", marginRight: "1rem" }}
         >
           Locate Me
         </button>
-      </div>
-      {showUser && (
-        <GoogleMapReact
-          bootstrapURLKeys={{
-            key: process.env.GOOGLE_MAP_KEY,
-          }}
-          defaultCenter={location.center}
-          defaultZoom={location.zoom}
-          center={location.center}
-          yesIWantToUseGoogleMapApiInternals={true}
-          onGoogleApiLoaded={({ map, maps }) => {
-            new maps.Circle({
-              strokeColor: "#FF0000",
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: "#FF0000",
-              fillOpacity: 0.3,
-              map,
-              center: { lat: location.center.lat, lng: location.center.lng },
-              radius: radius,
-            });
-          }}
+        <button className="search-user-btn"
+          onClick={showOthers}
         >
+          Show Other User
+        </button>
+
+        {/*Show radius on the map (WIP) */}
+        {/*<form>*/}
+        {/*  <input type="number" id="radius" name="radius" placeholder="radius in km" style={{width: "7rem"}} onChange={radiusHandler} />*/}
+        {/*  <br />*/}
+        {/*  <button style={{width: "7.5rem"}} onClick={changeRadius}>Change Radius</button>*/}
+        {/*</form>*/}
+      </div>
+
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: process.env.GOOGLE_MAP_KEY,
+        }}
+        defaultCenter={location.center}
+        defaultZoom={location.zoom}
+        center={location.center}
+        yesIWantToUseGoogleMapApiInternals={true}
+
+        // // Show radius on the map (WIP)
+        // onGoogleApiLoaded={({ map, maps }) => {
+        //   setMapReference(map);
+        //   setMapsReference(maps);
+        // }}
+      >
+        {showUser && (
           <img
             src={dummyImg}
             alt="Profile picture"
@@ -83,34 +161,20 @@ const GoogleMapsStatic = () => {
             lat={location.center.lat}
             lng={location.center.lng}
           />
-
-          <img
-            src={dummyImg}
-            alt="Profile picture"
-            style={{ width: "40px", height: "auto", borderRadius: "30%" }}
-            lat="52.52"
-            lng="13.405"
-          />
-        </GoogleMapReact>
-      )}
-      {!showUser && (
-        <GoogleMapReact
-          bootstrapURLKeys={{
-            key: process.env.GOOGLE_MAP_KEY,
-          }}
-          defaultCenter={location.center}
-          defaultZoom={location.zoom}
-          center={location.center}
-        >
+        )}
+        {showOtherUsers &&
+          otherUsersLocation.map((userData) => (
             <img
-                src={dummyImg}
-                alt="Profile picture"
-                style={{ width: "40px", height: "auto", borderRadius: "30%" }}
-                lat="52.52"
-                lng="13.405"
+              src={userData.picture}
+              alt="Profile picture"
+              style={{ width: "40px", height: "auto", borderRadius: "30%", cursor: "pointer" }}
+              lat={userData.latitude}
+              lng={userData.longitude}
+              onClick={() => {handleModal(userData.id)}}
             />
-        </GoogleMapReact>
-      )}
+          ))}
+      </GoogleMapReact>
+        <UserModalMap userId={userId} otherUsersLocation={otherUsersLocation} openUserModal={openUserModal} setOpenUserModal={setOpenUserModal} handleCloseUserModal={handleCloseUserModal} />
     </div>
   );
 };
